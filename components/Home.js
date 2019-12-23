ce1aa3fb02a6930b0add89643e1515683ac9b44b
Dan Eversen <dan_eversen@mckinsey.com>
2019-12-23 11:13:56 -0500

    add in first style, need to tweak autocorrect

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
 
 const Home = () => {
   return (
-    <>
+    <View style={styles.screenContainer}>
       <Text>HELLO WORLD</Text>
-    </>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    display: 'flex',
+    flex: 1,
+    width: '100%',
+  }
+});
 
 export default Home;
